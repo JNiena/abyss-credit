2392eacdff92c65d3d219d94415fbfa639273346
arnavpmr <arnavpmr@gmail.com>
2022-03-16 23:05:00 -0400

    Fixed some file names

--- preconditions/KingOnly.ts
+++ preconditions/KingOnly.ts
@@ -2,7 +2,7 @@ import { Precondition } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 import roles from '../roles';
 
-export class OwnerOnlyPrecondition extends Precondition {
+export class KingOnlyPrecondition extends Precondition {
   public run(message: Message) {
     return message.member?.roles.cache.has(roles.King)
       ? this.ok()
