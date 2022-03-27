2ea719875d81a086e3e29ca4b0910014c11e1cf1
JNiena <JNiena@protonmail.com>
2022-03-27 14:46:51 -0400

    Latest build

--- target/source/Main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const framework_1 = require("@sapphire/framework");
-const Bot_1 = __importDefault(require("../config/Bot"));
-let client = new framework_1.SapphireClient({ intents: ["GUILDS", "GUILD_MESSAGES"], defaultPrefix: Bot_1.default.prefix });
-client.login(Bot_1.default.token).then();
