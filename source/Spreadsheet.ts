fb2ae89d942873bae0bb527bf264c088091427fe
Jack Nienaber <JNiena@protonmail.com>
2022-12-03 04:27:45 -0500

    Added spreadsheet by title

--- source/Spreadsheet.ts
+++ source/Spreadsheet.ts
@@ -2,15 +2,15 @@ import { GoogleSpreadsheet, GoogleSpreadsheetRow, GoogleSpreadsheetWorksheet } f
 
 export class Spreadsheet {
 
-	public static async getRows(spreadSheetID: string, clientEmail: string, privateKey: string): Promise<GoogleSpreadsheetRow[]> {
-		return (await this.getSheet(spreadSheetID, clientEmail, privateKey)).getRows().then();
+	public static async getRows(spreadSheetID: string, spreadSheetTitle: string, clientEmail: string, privateKey: string): Promise<GoogleSpreadsheetRow[]> {
+		return (await this.getSheet(spreadSheetID, spreadSheetTitle, clientEmail, privateKey)).getRows().then();
 	}
 
-	public static async getSheet(spreadSheetID: string, clientEmail: string, privateKey: string): Promise<GoogleSpreadsheetWorksheet> {
+	public static async getSheet(spreadSheetID: string, spreadSheetTitle: string, clientEmail: string, privateKey: string): Promise<GoogleSpreadsheetWorksheet> {
 		let spreadsheet = new GoogleSpreadsheet(spreadSheetID);
 		await spreadsheet.useServiceAccountAuth({ client_email: clientEmail, private_key: privateKey });
 		await spreadsheet.loadInfo();
-		return spreadsheet.sheetsByIndex[0];
+		return spreadsheet.sheetsByTitle[spreadSheetTitle];
 	}
 
 }
