f969dd66a8cbb0d5d13c9a316a0cdaafef3716e7
Jack Nienaber <JNiena@protonmail.com>
2023-04-11 19:27:56 -0400

    Reformat

--- source/listeners/chatInputCommandDenied.ts
+++ source/listeners/chatInputCommandDenied.ts
@@ -1,8 +1,8 @@
-import { type ChatInputCommandDeniedPayload, Events, Listener, type UserError } from "@sapphire/framework";
-import { InteractionResponse, Message } from "discord.js";
+import {type ChatInputCommandDeniedPayload, Events, Listener, type UserError} from "@sapphire/framework";
+import {InteractionResponse, Message} from "discord.js";
 
 export class ChatInputCommandDenied extends Listener<typeof Events.ChatInputCommandDenied> {
-	public run(error: UserError, { interaction }: ChatInputCommandDeniedPayload): Promise<Message> | Promise<InteractionResponse> {
+	public run(error: UserError, {interaction}: ChatInputCommandDeniedPayload): Promise<Message> | Promise<InteractionResponse> {
 		if (interaction.deferred || interaction.replied) {
 			return interaction.editReply({
 				"content": error.message
