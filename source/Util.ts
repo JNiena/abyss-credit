aef3af14b1960797e8366bd0df167c0f9c392866
Jack Nienaber <JNiena@protonmail.com>
2023-04-11 19:23:14 -0400

    Added common currency choices method

--- source/Util.ts
+++ source/Util.ts
@@ -1,5 +1,18 @@
+import {SlashCommandStringOption} from "discord.js";
+import Config = require("./Config");
+
 export class Util {
 	public static capitalize(string: string): string {
 		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+	public static addCurrencyChoices(option: SlashCommandStringOption): SlashCommandStringOption {
+		for (let i = 0; i < Config.currencies.length; i++) {
+			option.addChoices({"name": Config.currencies[i], "value": Config.currencies[i]});
+		}
+		return option.addChoices({
+			"name": "all",
+			"value": "all"
+		});
 	}
 }
