df4add7a9796a7f3b6f9f51c8d2bf950410160a4
Jack Nienaber <JNiena@protonmail.com>
2023-04-11 19:22:56 -0400

    No longer needed

--- source/Timestamp.ts
+++
@@ -1,6 +0,0 @@
-export class Timestamp {
-	public static now(): string {
-		const date: Date = new Date();
-		return date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	}
-}
