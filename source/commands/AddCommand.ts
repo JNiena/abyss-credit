53f445cac1193a8e100809edbccdb24b55724ea4
Jack Nienaber <JNiena@protonmail.com>
2023-04-11 19:27:47 -0400

    Reformat. Uses IDs now

--- source/commands/AddCommand.ts
+++ source/commands/AddCommand.ts
@@ -1,8 +1,9 @@
-import { ChatInputCommand, Command } from "@sapphire/framework";
-import { ChatInputCommandInteraction, InteractionResponse, User } from "discord.js";
-import { CurrencySystem } from "../CurrencySystem";
-import { Embeds } from "../Embeds";
-import { Spreadsheet } from "../Spreadsheet";
+import {ChatInputCommand, Command} from "@sapphire/framework";
+import {ChatInputCommandInteraction, InteractionResponse, User} from "discord.js";
+import {CurrencySystem} from "../CurrencySystem";
+import {Embeds} from "../Embeds";
+import {Spreadsheet} from "../Spreadsheet";
+import {Util} from "../Util";
 import Config = require("../Config");
 
 export class AddCommand extends Command {
@@ -22,26 +23,22 @@ export class AddCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry): void {
 		registry.registerChatInputCommand(builder => {
 			builder
-			.setName(this.name)
-			.setDescription(this.description)
-			.addStringOption(option => {
-				for (let i = 0; i < Config.currencies.length; i++) {
-					option.addChoices({ "name": Config.currencies[i], "value": Config.currencies[i] });
-				}
-				return option.addChoices({ "name": "all", "value": "all" }).setName("currency").setDescription("The type of currency to add.").setRequired(true);
-			})
-			.addIntegerOption(option => option.setName("amount").setDescription("The amount of currency to add.").setRequired(true).setMinValue(1))
-			.addUserOption(option => option.setName("user").setDescription("The user to add currency to.").setRequired(true))
-			.addStringOption(option => option.setName("reason").setDescription("The reason for adding currency.").setRequired(true).setMinLength(1));
-		}, { idHints: ["1093782220554129408"] });
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption(option => Util.addCurrencyChoices(option).setName("currency").setDescription("The type of currency to add.").setRequired(true))
+				.addIntegerOption(option => option.setName("amount").setDescription("The amount of currency to add.").setRequired(true).setMinValue(1))
+				.addUserOption(option => option.setName("user").setDescription("The user to add currency to.").setRequired(true))
+				.addStringOption(option => option.setName("reason").setDescription("The reason for adding currency.").setRequired(true).setMinLength(1));
+		}, {"idHints": ["1093782220554129408"]});
 	}
 
 	public override async chatInputRun(interaction: ChatInputCommandInteraction, context: ChatInputCommand.RunContext): Promise<InteractionResponse> {
+		await interaction.deferReply();
 		const currency: string = interaction.options.getString("currency", true);
 		const amount: number = interaction.options.getInteger("amount", true);
 		const user: User = interaction.options.getUser("user", true);
 		const reason: string = interaction.options.getString("reason", true);
-		await this.currencySystem.add(currency, interaction.user.username, user.username, amount, reason);
-		return interaction.reply({ "embeds": [Embeds.added(Embeds.currency(currency, amount), currency, amount, user, reason)] });
+		await this.currencySystem.add(currency, interaction.user.id, user.id, amount, reason);
+		return interaction.editReply({"embeds": [Embeds.added(Embeds.currency(currency, amount), currency, amount, user, reason)]}).then();
 	}
 }
