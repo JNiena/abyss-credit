12a30b8f82d410f56381c44a56a1925966d19573
Jack Nienaber <JNiena@protonmail.com>
2023-04-11 19:23:43 -0400

    Reformat

--- source/Main.ts
+++ source/Main.ts
@@ -1,12 +1,16 @@
-import { LogLevel, SapphireClient } from "@sapphire/framework";
-import { GatewayIntentBits } from "discord.js";
+import {LogLevel, SapphireClient} from "@sapphire/framework";
+import {GatewayIntentBits} from "discord.js";
 import Config = require("./Config");
 
 const client: SapphireClient = new SapphireClient({
-	"intents": [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+	"intents": [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	],
 	"loadMessageCommandListeners": true,
 	"loadDefaultErrorListeners": true,
-	"logger": { "level": LogLevel.Debug }
+	"logger": {"level": LogLevel.Debug}
 });
 
 client.login(Config.token).then();
