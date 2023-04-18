fad53cea02c172d6fe24b3a4a5c042909666f792
Jack Nienaber <JNiena@protonmail.com>
2023-04-18 10:07:50 -0400

    Added custom cooldown message

--- source/listeners/chatInputCommandDenied.ts
+++ source/listeners/chatInputCommandDenied.ts
@@ -1,15 +1,21 @@
-import {type ChatInputCommandDeniedPayload, Events, Listener, type UserError} from "@sapphire/framework";
+import {type ChatInputCommandDeniedPayload, Events, Identifiers, Listener, type UserError} from "@sapphire/framework";
 import {InteractionResponse, Message} from "discord.js";
+import {DurationFormatter} from "@sapphire/time-utilities";
 
 export class ChatInputCommandDenied extends Listener<typeof Events.ChatInputCommandDenied> {
 	public run(error: UserError, {interaction}: ChatInputCommandDeniedPayload): Promise<Message> | Promise<InteractionResponse> {
+		let message: string = error.message;
+		if (error.identifier === Identifiers.PreconditionCooldown) {
+			let remaining = new DurationFormatter().format(Reflect.get(Object(error.context), "remaining"));
+			message = `This command is still on a cooldown! Try again in ${remaining}.`;
+		}
 		if (interaction.deferred || interaction.replied) {
 			return interaction.editReply({
-				"content": error.message
+				"content": message
 			});
 		}
 		return interaction.reply({
-			"content": error.message,
+			"content": message,
 			"ephemeral": true
 		});
 	}
