285801f3e8667c5f1bddfa5d6e12f5c497ef2097
Jack Nienaber <JNiena@protonmail.com>
2023-05-04 19:41:39 -0400

    Changed config access method

--- source/listeners/chatInputCommandDenied.ts
+++ source/listeners/chatInputCommandDenied.ts
@@ -3,20 +3,20 @@ import {InteractionResponse, Message} from "discord.js";
 import {DurationFormatter} from "@sapphire/time-utilities";
 
 export class ChatInputCommandDenied extends Listener<typeof Events.ChatInputCommandDenied> {
-	public run(error: UserError, {interaction}: ChatInputCommandDeniedPayload): Promise<Message> | Promise<InteractionResponse> {
-		let message: string = error.message;
-		if (error.identifier === Identifiers.PreconditionCooldown) {
-			const remaining = new DurationFormatter().format(Reflect.get(Object(error.context), "remaining"));
-			message = `This command is still on a cooldown! Try again in ${remaining}.`;
-		}
-		if (interaction.deferred || interaction.replied) {
-			return interaction.editReply({
-				"content": message
-			});
-		}
-		return interaction.reply({
-			"content": message,
-			"ephemeral": true
-		});
-	}
+    public run(error: UserError, {interaction}: ChatInputCommandDeniedPayload): Promise<Message> | Promise<InteractionResponse> {
+        let message: string = error.message;
+        if (error.identifier === Identifiers.PreconditionCooldown) {
+            const remaining = new DurationFormatter().format(Reflect.get(Object(error.context), "remaining"));
+            message = `This command is still on a cooldown! Try again in ${remaining}.`;
+        }
+        if (interaction.deferred || interaction.replied) {
+            return interaction.editReply({
+                "content": message
+            });
+        }
+        return interaction.reply({
+            "content": message,
+            "ephemeral": true
+        });
+    }
 }
