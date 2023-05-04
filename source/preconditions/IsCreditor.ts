888d7e2a4d6b106832006c7577bb6c3ac0a80398
Jack Nienaber <JNiena@protonmail.com>
2023-05-04 19:41:25 -0400

    Changed config access method

--- source/preconditions/IsCreditor.ts
+++ source/preconditions/IsCreditor.ts
@@ -2,25 +2,25 @@ import {Precondition, Result, UserError} from "@sapphire/framework";
 import {CommandInteraction, ContextMenuCommandInteraction, GuildMemberRoleManager, Message} from "discord.js";
 
 export class IsCreditor extends Precondition {
-	public override async messageRun(message: Message): Promise<Result<unknown, UserError>> {
-		return this.error({"message": "This precondition is not supported."});
-	}
+    public override async messageRun(message: Message): Promise<Result<unknown, UserError>> {
+        return this.error({"message": "This precondition is not supported."});
+    }
 
-	public override async chatInputRun(interaction: CommandInteraction): Promise<Result<unknown, UserError>> {
-		return this.hasCreditorRole(interaction.member?.roles);
-	}
+    public override async chatInputRun(interaction: CommandInteraction): Promise<Result<unknown, UserError>> {
+        return this.hasCreditorRole(interaction.member?.roles);
+    }
 
-	public override async contextMenuRun(interaction: ContextMenuCommandInteraction): Promise<Result<unknown, UserError>> {
-		return this.hasCreditorRole(interaction.member?.roles);
-	}
+    public override async contextMenuRun(interaction: ContextMenuCommandInteraction): Promise<Result<unknown, UserError>> {
+        return this.hasCreditorRole(interaction.member?.roles);
+    }
 
-	private async hasCreditorRole(roles: GuildMemberRoleManager | string[] | null | undefined): Promise<Result<unknown, UserError>> {
-		const message: string = "You need the creditor role to assign currencies.";
-		if (!roles || !(roles instanceof GuildMemberRoleManager)) {
-			return this.error({"message": message});
-		}
-		return roles.member.roles.cache.some(role => role.name === "Creditor")
-			? this.ok()
-			: this.error({"message": message});
-	}
+    private async hasCreditorRole(roles: GuildMemberRoleManager | string[] | null | undefined): Promise<Result<unknown, UserError>> {
+        const message: string = "You need the creditor role to assign currencies.";
+        if (!roles || !(roles instanceof GuildMemberRoleManager)) {
+            return this.error({"message": message});
+        }
+        return roles.member.roles.cache.some(role => role.name === "Creditor")
+            ? this.ok()
+            : this.error({"message": message});
+    }
 }
--- source/preconditions/IsValidChannel.ts
+++ source/preconditions/IsValidChannel.ts
@@ -1,24 +1,23 @@
 import {Precondition, Result, UserError} from "@sapphire/framework";
 import {CommandInteraction, ContextMenuCommandInteraction, Message} from "discord.js";
-import Config = require("../Config");
+import {config} from "../Main";
 
 export class IsValidChannel extends Precondition {
-	public override async messageRun(message: Message): Promise<Result<unknown, UserError>> {
-		return this.error({"message": "This precondition is not supported."});
-	}
+    public override async messageRun(message: Message): Promise<Result<unknown, UserError>> {
+        return this.error({"message": "This precondition is not supported."});
+    }
 
-	public override async chatInputRun(interaction: CommandInteraction): Promise<Result<unknown, UserError>> {
-		return this.isValidChannel(interaction.channelId);
-	}
+    public override async chatInputRun(interaction: CommandInteraction): Promise<Result<unknown, UserError>> {
+        return this.isValidChannel(interaction.channelId);
+    }
 
-	public override async contextMenuRun(interaction: ContextMenuCommandInteraction): Promise<Result<unknown, UserError>> {
-		return this.isValidChannel(interaction.channelId);
-	}
+    public override async contextMenuRun(interaction: ContextMenuCommandInteraction): Promise<Result<unknown, UserError>> {
+        return this.isValidChannel(interaction.channelId);
+    }
 
-	private async isValidChannel(channelId: string): Promise<Result<unknown, UserError>> {
-		// @ts-ignore
-		return Config.channels.includes(channelId)
-			? this.ok()
-			: this.error({"message": "You cannot use that command in this channel."});
-	}
+    private async isValidChannel(channelId: string): Promise<Result<unknown, UserError>> {
+        return config.get().channels.includes(channelId)
+            ? this.ok()
+            : this.error({"message": "You cannot use that command in this channel."});
+    }
 }
