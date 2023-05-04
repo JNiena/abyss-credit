497c237e0a0f58f329692fb6ae3242547da19486
Jack Nienaber <JNiena@protonmail.com>
2023-05-04 19:05:17 -0400

    Added level embed

--- source/Embeds.ts
+++ source/Embeds.ts
@@ -3,88 +3,107 @@ import {Util} from "./Util";
 import Config = require("./Config");
 
 export class Embeds {
-	public static daily(fields: any, user: User): any {
-		return {
-			"title": "Daily Rewards",
-			"color": 7803166,
-			"fields": fields,
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
-	}
+    public static level(user: User, xp: number, totalXp: number, level: number): any {
+        return {
+            "title": "Level " + level,
+            "color": 7803166,
+            "fields": [
+                {
+                    "inline": true,
+                    "name": "Progress",
+                    "value": `\`${xp}/${totalXp}\`\n\`${Util.createProgressBar(xp, totalXp, 15)}\``
+                }
+            ],
+            "footer": {
+                "text": `User: ${user.username}`
+            },
+            "thumbnail": {
+                "url": user.displayAvatarURL()
+            }
+        };
+    }
 
-	public static balance(fields: any, user: User): any {
-		return {
-			"title": "Balance",
-			"color": 7803166,
-			"fields": fields,
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
-	}
+    public static daily(fields: any, user: User): any {
+        return {
+            "title": "Daily Rewards",
+            "color": 7803166,
+            "fields": fields,
+            "footer": {
+                "text": `User: ${user.username}`
+            },
+            "thumbnail": {
+                "url": user.displayAvatarURL()
+            }
+        };
+    }
 
-	public static added(fields: any, currency: string, amount: number, user: User, reason: string): any {
-		return {
-			"title": "Added",
-			"color": 7803166,
-			"fields": [...fields, {
-				"inline": false,
-				"name": "Reason",
-				"value": reason
-			}],
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
-	}
+    public static balance(fields: any, user: User): any {
+        return {
+            "title": "Balance",
+            "color": 7803166,
+            "fields": fields,
+            "footer": {
+                "text": `User: ${user.username}`
+            },
+            "thumbnail": {
+                "url": user.displayAvatarURL()
+            }
+        };
+    }
 
-	public static removed(fields: any, currency: string, amount: number, user: User, reason: string): any {
-		return {
-			"title": "Removed",
-			"color": 7803166,
-			"fields": [...fields, {
-				"inline": false,
-				"name": "Reason",
-				"value": reason
-			}],
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
-	}
+    public static added(fields: any, currency: string, amount: number, user: User, reason: string): any {
+        return {
+            "title": "Added",
+            "color": 7803166,
+            "fields": [...fields, {
+                "inline": false,
+                "name": "Reason",
+                "value": reason
+            }],
+            "footer": {
+                "text": `User: ${user.username}`
+            },
+            "thumbnail": {
+                "url": user.displayAvatarURL()
+            }
+        };
+    }
 
-	public static currency(currency: string, amount: number, symbol: string): any {
-		let fields: { "inline": boolean, "name": string, "value": string }[] = [];
-		if (currency === "all") {
-			for (let i = 0; i < Config.currencies.length; i++) {
-				fields.push(this.entry(Config.currencies[i], amount, symbol));
-			}
-		}
-		else {
-			fields.push(this.entry(currency, amount));
-		}
-		return fields;
-	}
+    public static removed(fields: any, currency: string, amount: number, user: User, reason: string): any {
+        return {
+            "title": "Removed",
+            "color": 7803166,
+            "fields": [...fields, {
+                "inline": false,
+                "name": "Reason",
+                "value": reason
+            }],
+            "footer": {
+                "text": `User: ${user.username}`
+            },
+            "thumbnail": {
+                "url": user.displayAvatarURL()
+            }
+        };
+    }
 
-	public static entry(currency: string, amount: number, symbol: string = ""): any {
-		return {
-			"inline": true,
-			"name": Util.capitalize(currency),
-			"value": `[${symbol}${amount.toString()}](https://localhost)`
-		};
-	}
+    public static currency(currency: string, amount: number, symbol: string): any {
+        let fields: { "inline": boolean, "name": string, "value": string }[] = [];
+        if (currency === "all") {
+            for (let i = 0; i < Config.currencies.length; i++) {
+                fields.push(this.entry(Config.currencies[i], amount, symbol));
+            }
+        } else {
+            fields.push(this.entry(currency, amount));
+        }
+        return fields;
+    }
+
+    public static entry(currency: string, amount: number, symbol: string = ""): any {
+        return {
+            "inline": true,
+            "name": Util.capitalize(currency),
+            "value": `[${symbol}${amount.toString()}](https://localhost)`
+        };
+    }
 }
