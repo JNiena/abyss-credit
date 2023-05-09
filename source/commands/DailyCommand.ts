b91c0599eca378ad89300b7182613b2b5ddfb830
Jack Nienaber <JNiena@protonmail.com>
2023-05-08 22:09:17 -0400

    Reformat

--- source/commands/DailyCommand.ts
+++ source/commands/DailyCommand.ts
@@ -1,41 +1,36 @@
-import {ChatInputCommand, Command} from "@sapphire/framework";
-import {ChatInputCommandInteraction, InteractionResponse} from "discord.js";
-import {CurrencySystem} from "../CurrencySystem";
-import {Embeds} from "../Embeds";
-import {Spreadsheet} from "../Spreadsheet";
-import {config} from "../Main";
+import { ChatInputCommand, Command } from "@sapphire/framework";
+import { ChatInputCommandInteraction, InteractionResponse } from "discord.js";
+import { Embeds } from "../Embeds";
+import { config, currencySystem } from "../Main";
 
 export class DailyCommand extends Command {
-    private currencySystem: CurrencySystem;
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, {
+			...options,
+			"name": "daily",
+			// @ts-ignore
+			"preconditions": ["IsValidChannel"],
+			"cooldownDelay": 86_400_000,
+			"description": "Rewards up to three credits and gold every day."
+		});
+	}
 
-    public constructor(context: Command.Context, options: Command.Options) {
-        super(context, {
-            ...options,
-            "name": "daily",
-            // @ts-ignore
-            "preconditions": ["IsValidChannel"],
-            "cooldownDelay": 86_400_000,
-            "description": "Rewards up to three credits and gold every day."
-        });
-        this.currencySystem = new CurrencySystem(new Spreadsheet(config.get().spreadsheet.id, config.get().spreadsheet.clientEmail, config.get().spreadsheet.privateKey));
-    }
+	public override registerApplicationCommands(registry: Command.Registry): void {
+		registry.registerChatInputCommand(builder => {
+			builder
+				.setName(this.name)
+				.setDescription(this.description);
+		}, { "idHints": ["1093782138891014205"] });
+	}
 
-    public override registerApplicationCommands(registry: Command.Registry): void {
-        registry.registerChatInputCommand(builder => {
-            builder
-                .setName(this.name)
-                .setDescription(this.description);
-        }, {"idHints": ["1093782138891014205"]});
-    }
-
-    public override async chatInputRun(interaction: ChatInputCommandInteraction, context: ChatInputCommand.RunContext): Promise<InteractionResponse> {
-        await interaction.deferReply();
-        const fields: { "inline": boolean, "name": string, "value": string }[] = [];
-        for (let i = 0; i < config.get().currencies.length; i++) {
-            const amount: number = Math.floor(Math.random() * 3) + 1;
-            await this.currencySystem.add(config.get().currencies[i], "Daily", interaction.user.id, amount, "Daily");
-            fields.push(Embeds.entry(config.get().currencies[i], amount, "+"));
-        }
-        return interaction.editReply({"embeds": [Embeds.daily(fields, interaction.user)]}).then();
-    }
+	public override async chatInputRun(interaction: ChatInputCommandInteraction, context: ChatInputCommand.RunContext): Promise<InteractionResponse> {
+		await interaction.deferReply();
+		const fields: { "inline": boolean, "name": string, "value": string }[] = [];
+		for (let i = 0; i < config.get().currencies.length; i++) {
+			const amount: number = Math.floor(Math.random() * 3) + 1;
+			await currencySystem.add(config.get().currencies[i], "Daily", interaction.user.id, amount, "Daily");
+			fields.push(Embeds.entry(config.get().currencies[i], amount, "+"));
+		}
+		return interaction.editReply({ "embeds": [Embeds.daily(fields, interaction.user)] }).then();
+	}
 }
--- source/commands/RemoveCommand.ts
+++ source/commands/RemoveCommand.ts
@@ -1,44 +1,39 @@
-import {ChatInputCommand, Command} from "@sapphire/framework";
-import {ChatInputCommandInteraction, InteractionResponse, User} from "discord.js";
-import {CurrencySystem} from "../CurrencySystem";
-import {Embeds} from "../Embeds";
-import {Spreadsheet} from "../Spreadsheet";
-import {Util} from "../Util";
-import {config} from "../Main";
+import { ChatInputCommand, Command } from "@sapphire/framework";
+import { ChatInputCommandInteraction, InteractionResponse, User } from "discord.js";
+import { Embeds } from "../Embeds";
+import { currencySystem } from "../Main";
+import { Util } from "../Util";
 
 export class RemoveCommand extends Command {
-    private currencySystem: CurrencySystem;
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, {
+			...options,
+			"name": "remove",
+			// @ts-ignore
+			"preconditions": ["IsCreditor", "IsValidChannel"],
+			"description": "Removes from the balance of the designated user."
+		});
+	}
 
-    public constructor(context: Command.Context, options: Command.Options) {
-        super(context, {
-            ...options,
-            "name": "remove",
-            // @ts-ignore
-            "preconditions": ["IsCreditor", "IsValidChannel"],
-            "description": "Removes from the balance of the designated user."
-        });
-        this.currencySystem = new CurrencySystem(new Spreadsheet(config.get().spreadsheet.id, config.get().spreadsheet.clientEmail, config.get().spreadsheet.privateKey));
-    }
+	public override registerApplicationCommands(registry: Command.Registry): void {
+		registry.registerChatInputCommand(builder => {
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption(option => Util.addCurrencyChoices(option).setName("currency").setDescription("The type of currency to remove.").setRequired(true))
+				.addIntegerOption(option => option.setName("amount").setDescription("The amount of currency to remove.").setRequired(true).setMinValue(1))
+				.addUserOption(option => option.setName("user").setDescription("The user to remove currency from.").setRequired(true))
+				.addStringOption(option => option.setName("reason").setDescription("The reason for removing currency.").setRequired(true));
+		}, { "idHints": ["1093782137724993547"] });
+	}
 
-    public override registerApplicationCommands(registry: Command.Registry): void {
-        registry.registerChatInputCommand(builder => {
-            builder
-                .setName(this.name)
-                .setDescription(this.description)
-                .addStringOption(option => Util.addCurrencyChoices(option).setName("currency").setDescription("The type of currency to remove.").setRequired(true))
-                .addIntegerOption(option => option.setName("amount").setDescription("The amount of currency to remove.").setRequired(true).setMinValue(1))
-                .addUserOption(option => option.setName("user").setDescription("The user to remove currency from.").setRequired(true))
-                .addStringOption(option => option.setName("reason").setDescription("The reason for removing currency.").setRequired(true));
-        }, {"idHints": ["1093782137724993547"]});
-    }
-
-    public override async chatInputRun(interaction: ChatInputCommandInteraction, context: ChatInputCommand.RunContext): Promise<InteractionResponse> {
-        await interaction.deferReply();
-        const currency: string = interaction.options.getString("currency", true);
-        const amount: number = interaction.options.getInteger("amount", true);
-        const user: User = interaction.options.getUser("user", true);
-        const reason: string = interaction.options.getString("reason", true);
-        await this.currencySystem.remove(currency, interaction.user.id, user.id, amount, reason);
-        return interaction.editReply({"embeds": [Embeds.removed(Embeds.currency(currency, amount, "-"), currency, amount, user, reason)]}).then();
-    }
+	public override async chatInputRun(interaction: ChatInputCommandInteraction, context: ChatInputCommand.RunContext): Promise<InteractionResponse> {
+		await interaction.deferReply();
+		const currency: string = interaction.options.getString("currency", true);
+		const amount: number = interaction.options.getInteger("amount", true);
+		const user: User = interaction.options.getUser("user", true);
+		const reason: string = interaction.options.getString("reason", true);
+		await currencySystem.remove(currency, interaction.user.id, user.id, amount, reason);
+		return interaction.editReply({ "embeds": [Embeds.removed(Embeds.currency(currency, amount, "-"), currency, amount, user, reason)] }).then();
+	}
 }
