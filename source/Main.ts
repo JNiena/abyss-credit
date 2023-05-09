caf7465265eaef452c760215f15f4b49e85c58fa
Jack Nienaber <JNiena@protonmail.com>
2023-05-09 17:36:36 -0400

    Added discord client class

--- source/Main.ts
+++ source/Main.ts
@@ -1,7 +1,8 @@
-import { LogLevel, SapphireClient } from "@sapphire/framework";
+import { LogLevel } from "@sapphire/framework";
 import { GatewayIntentBits } from "discord.js";
 import { Config } from "./Config";
 import { CurrencySystem } from "./CurrencySystem";
+import { DiscordClient } from "./DiscordClient";
 import { LevelSystem } from "./LevelSystem";
 import { Spreadsheet } from "./Spreadsheet";
 
@@ -9,7 +10,7 @@ const config: Config = new Config("config.json");
 const levelSystem: LevelSystem = new LevelSystem();
 const currencySystem: CurrencySystem = new CurrencySystem(new Spreadsheet(config.get().spreadsheet.id, config.get().spreadsheet.clientEmail, config.get().spreadsheet.privateKey));
 
-const client: SapphireClient = new SapphireClient({
+const discordClient: DiscordClient = new DiscordClient({
 	"intents": [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
@@ -19,10 +20,11 @@ const client: SapphireClient = new SapphireClient({
 	"loadDefaultErrorListeners": true,
 	"logger": { "level": LogLevel.Debug }
 });
-client.login(config.get()["token"]).then();
+discordClient.login(config.get()["token"]).then();
 
 export {
 	config,
 	levelSystem,
-	currencySystem
+	currencySystem,
+	discordClient
 };
