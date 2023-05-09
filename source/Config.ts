1185fe49f43de4d71f42b97c87eaedb1ae3396da
Jack Nienaber <JNiena@protonmail.com>
2023-05-08 22:06:28 -0400

    Reformat

--- source/Config.ts
+++ source/Config.ts
@@ -1,26 +1,26 @@
-import {Files} from "./Files";
+import { Files } from "./Files";
 
 export class Config {
-    private data: any;
-    private path: string | undefined;
+	private data: any;
+	private path: string | undefined;
 
-    public constructor(path: string) {
-        this.path = path;
-        this.data = JSON.parse(Files.read(path));
-    }
+	public constructor(path: string) {
+		this.path = path;
+		this.data = JSON.parse(Files.read(path));
+	}
 
-    public get(): any {
-        return this.data;
-    }
+	public get(): any {
+		return this.data;
+	}
 
-    public set(data: any): void {
-        this.data = data;
-    }
+	public set(data: any): void {
+		this.data = data;
+	}
 
-    public save(): void {
-        if (this.path !== undefined) {
-            Files.delete(this.path);
-            Files.write(this.path, JSON.stringify(this.data, null, "\t"));
-        }
-    }
+	public save(): void {
+		if (this.path) {
+			Files.delete(this.path);
+			Files.write(this.path, JSON.stringify(this.data, null, "\t"));
+		}
+	}
 }
