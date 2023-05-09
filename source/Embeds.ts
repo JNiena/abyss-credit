a6abe3e6852b7b435e28dd157c1f7e4fbebca243
Jack Nienaber <JNiena@protonmail.com>
2023-05-09 16:28:28 -0400

    Created template embed

--- source/Embeds.ts
+++ source/Embeds.ts
@@ -1,94 +1,60 @@
-import { APIEmbed, User } from "discord.js";
+import { APIEmbed, APIEmbedField, User } from "discord.js";
 import { config } from "./Main";
 import { Util } from "./Util";
 
 export class Embeds {
+	public static readonly green: number = 2817792;
+	public static readonly red: number = 16711680;
+	public static readonly neutral: number = 7803166;
+
+	public static template(title: string, fields: APIEmbedField[], color: number, user: User): APIEmbed {
+		return {
+			"title": title,
+			"color": color,
+			"fields": fields,
+			"footer": {
+				"text": `User: ${user.username}`
+			},
+			"thumbnail": {
+				"url": user.displayAvatarURL()
+			}
+		};
+	}
+
 	public static level(user: User, xp: number, totalXp: number, level: number): APIEmbed {
-		return {
-			"title": "Level " + level,
-			"color": 7803166,
-			"fields": [
-				{
-					"inline": true,
-					"name": "Progress",
-					"value": `\`${xp}/${totalXp}\`\n\`${Util.createProgressBar(xp, totalXp, 15)}\``
-				}
-			],
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
+		return this.template("Level" + level, [{
+			"name": "Progress",
+			"value": `\`${xp}/${totalXp}\`\n\`${Util.createProgressBar(xp, totalXp, 15)}\``,
+			"inline": true
+		}], this.neutral, user);
 	}
 
-	public static daily(fields: any, user: User): APIEmbed {
-		return {
-			"title": "Daily Rewards",
-			"color": 7803166,
-			"fields": fields,
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
+	public static added(fields: APIEmbedField[], currency: string, amount: number, user: User, reason: string): APIEmbed {
+		return this.template("Added", [...fields, {
+			"name": "Reason",
+			"value": reason,
+			"inline": false
+		}], this.neutral, user);
 	}
 
-	public static balance(fields: any, user: User): APIEmbed {
-		return {
-			"title": "Balance",
-			"color": 7803166,
-			"fields": fields,
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
+	public static removed(fields: APIEmbedField[], currency: string, amount: number, user: User, reason: string): APIEmbed {
+		return this.template("Removed", [...fields, {
+			"name": "Reason",
+			"value": reason,
+			"inline": false
+		}], this.neutral, user);
 	}
 
-	public static added(fields: any, currency: string, amount: number, user: User, reason: string): APIEmbed {
-		return {
-			"title": "Added",
-			"color": 7803166,
-			"fields": [...fields, {
-				"inline": false,
-				"name": "Reason",
-				"value": reason
-			}],
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
+	public static balance(fields: APIEmbedField[], user: User): APIEmbed {
+		return this.template("Balance", fields, this.neutral, user);
 	}
 
-	public static removed(fields: any, currency: string, amount: number, user: User, reason: string): APIEmbed {
-		return {
-			"title": "Removed",
-			"color": 7803166,
-			"fields": [...fields, {
-				"inline": false,
-				"name": "Reason",
-				"value": reason
-			}],
-			"footer": {
-				"text": `User: ${user.username}`
-			},
-			"thumbnail": {
-				"url": user.displayAvatarURL()
-			}
-		};
+	public static daily(fields: APIEmbedField[], user: User): APIEmbed {
+		return this.template("Daily Rewards", fields, this.neutral, user);
 	}
 
-	public static currency(currency: string, amount: number, symbol: string): { "inline": boolean, "name": string, "value": string }[] {
-		let fields: { "inline": boolean, "name": string, "value": string }[] = [];
+	public static currency(currency: string, amount: number, symbol: string): APIEmbedField[] {
+		let fields: APIEmbedField[] = [];
 		if (currency === "all") {
 			for (let i = 0; i < config.get().currencies.length; i++) {
 				fields.push(this.entry(config.get().currencies[i], amount, symbol));
@@ -100,11 +66,11 @@ export class Embeds {
 		return fields;
 	}
 
-	public static entry(currency: string, amount: number, symbol: string = ""): { "inline": boolean, "name": string, "value": string } {
+	public static entry(currency: string, amount: number, symbol: string = ""): APIEmbedField {
 		return {
-			"inline": true,
 			"name": Util.capitalize(currency),
-			"value": `[${symbol}${amount.toString()}](https://localhost)`
+			"value": `[${symbol}${amount.toString()}](https://localhost)`,
+			"inline": true
 		};
 	}
 }
