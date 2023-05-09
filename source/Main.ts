9c39132735377a3a682f0a84b9ebd1d0520a2c89
Jack Nienaber <JNiena@protonmail.com>
2023-05-08 22:05:12 -0400

    Reformat

--- source/Main.ts
+++ source/Main.ts
@@ -1,24 +1,28 @@
-import {LogLevel, SapphireClient} from "@sapphire/framework";
-import {GatewayIntentBits} from "discord.js";
-import {Config} from "./Config";
+import { LogLevel, SapphireClient } from "@sapphire/framework";
+import { GatewayIntentBits } from "discord.js";
+import { Config } from "./Config";
+import { CurrencySystem } from "./CurrencySystem";
+import { LevelSystem } from "./LevelSystem";
+import { Spreadsheet } from "./Spreadsheet";
 
 const config: Config = new Config("config.json");
-const users: Config = new Config("users.json");
+const levelSystem: LevelSystem = new LevelSystem();
+const currencySystem: CurrencySystem = new CurrencySystem(new Spreadsheet(config.get().spreadsheet.id, config.get().spreadsheet.clientEmail, config.get().spreadsheet.privateKey));
 
 const client: SapphireClient = new SapphireClient({
-    "intents": [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ],
-    "loadMessageCommandListeners": true,
-    "loadDefaultErrorListeners": true,
-    "logger": {"level": LogLevel.Debug}
+	"intents": [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	],
+	"loadMessageCommandListeners": true,
+	"loadDefaultErrorListeners": true,
+	"logger": { "level": LogLevel.Debug }
 });
-
 client.login(config.get()["token"]).then();
 
 export {
-    config,
-    users
-}
+	config,
+	levelSystem,
+	currencySystem
+};
--- source/Spreadsheet.ts
+++ source/Spreadsheet.ts
@@ -1,23 +1,23 @@
-import {GoogleSpreadsheet, GoogleSpreadsheetRow, GoogleSpreadsheetWorksheet} from "google-spreadsheet";
+import { GoogleSpreadsheet, GoogleSpreadsheetRow, GoogleSpreadsheetWorksheet } from "google-spreadsheet";
 
 export class Spreadsheet {
-    private sheet: GoogleSpreadsheet;
-    private clientEmail: string;
-    private privateKey: string;
+	private sheet: GoogleSpreadsheet;
+	private clientEmail: string;
+	private privateKey: string;
 
-    public constructor(id: string, clientEmail: string, privateKey: string) {
-        this.sheet = new GoogleSpreadsheet(id);
-        this.clientEmail = clientEmail;
-        this.privateKey = privateKey.split(String.raw`\n`).join("\n");
-    }
+	public constructor(id: string, clientEmail: string, privateKey: string) {
+		this.sheet = new GoogleSpreadsheet(id);
+		this.clientEmail = clientEmail;
+		this.privateKey = privateKey.split(String.raw`\n`).join("\n");
+	}
 
-    public async worksheet(title: string): Promise<GoogleSpreadsheetWorksheet> {
-        await this.sheet.useServiceAccountAuth({client_email: this.clientEmail, private_key: this.privateKey});
-        await this.sheet.loadInfo();
-        return this.sheet.sheetsByTitle[title];
-    }
+	public async worksheet(title: string): Promise<GoogleSpreadsheetWorksheet> {
+		await this.sheet.useServiceAccountAuth({ client_email: this.clientEmail, private_key: this.privateKey });
+		await this.sheet.loadInfo();
+		return this.sheet.sheetsByTitle[title];
+	}
 
-    public async rows(title: string): Promise<GoogleSpreadsheetRow[]> {
-        return (await this.worksheet(title)).getRows().then();
-    }
+	public async rows(title: string): Promise<GoogleSpreadsheetRow[]> {
+		return (await this.worksheet(title)).getRows().then();
+	}
 }
